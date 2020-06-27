7873ca0f2e7894c0b1aafbf1a67e51c365f643e9
Longqian Huang <longqianh@gmail.com>
2020-06-27 12:09:00 +0800

    Create i_fft.m

---
+++ 3d-gray/i_fft.m
@@ -0,0 +1,9 @@
+function Uf=i_fft(M,N,lambda,d,xx0,yy0,xx,yy,Phase)
+k=2*pi/lambda;
+U0=exp(1i*Phase-pi);
+Fresnel=exp(-1i*k/2/d*(xx.^2+yy.^2));
+f2=U0.*Fresnel;
+Uf=ifft2(f2,N,M);% ÊÇ·ñÍü¼Çfftshift£¿£¿
+phase=exp(-1i*k*d)/(-1i*lambda*d)*exp(-1i*k/2/d*(xx0.^2+yy0.^2));
+Uf=Uf.*phase;
+end
