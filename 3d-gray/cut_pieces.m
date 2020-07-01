8800dcd944bad3222751c3661f2ee9d5b46836c9
Longqian Huang <longqianh@gmail.com>
2020-07-01 13:08:47 +0800

    Update cut_pieces.m

--- 3d-gray/cut_pieces.m
+++ 3d-gray/cut_pieces.m
@@ -1,38 +1,38 @@
-% input:number of slices n
-% cut pieces
-
 function cut_pieces(model,slices)
 
+% Cut pieces from 3-D objects.
+% INPUT: number of slices n
+% OUT: write images of slices into tmp file
+
+
 %sort by z coordinate
-% ver=sortrows(model.Location,3);
 ver=sortrows(model,3);
 X=ver(:,1);
 Y=ver(:,2);
-Z=ver(:,3);
 num=size(X,1);
-
-% adjust z coordinate
-min_z=min(ver(:,3));
-ver(:,3)=ver(:,3)-min_z;
-% depth=ver(end,3)*1000;
-% get z-range of one slice per layer
-ver1=ver(1:floor(num/slices):num,:);
-z1=ver1(:,3);
-
 XMIN=min(X);
 XMAX=max(X);
 YMIN=min(Y);
 YMAX=max(Y);
 
+% adjust z coordinate
+min_z=min(ver(:,3));
+ver(:,3)=ver(:,3)-min_z;
+
+% get z-range of one slice per layer
+ver1=ver(1:floor(num/slices):num,:);
+z1=ver1(:,3);
+
 for i = 1:length(z1)
     if i<length(z1) 
      % add all the points whose z within the z-range
-    xy=ver(ver(:,3)>z1(i)&ver(:,3)<z1(i+1),1:2);   
+        xy=ver(ver(:,3)>z1(i)&ver(:,3)<z1(i+1),1:2);   
     else % the last z-range
         xy=ver(ver(:,3)>z1(i),1:2);
     end
-    
-    scatter(xy(:,1),xy(:,2),10,'w','filled'); % scatter VS plot sz=9 
+%     color=rand(length(xy(:,1)),3);
+%     scatter(xy(:,1),xy(:,2),10,color,'filled'); 
+    scatter(xy(:,1),xy(:,2),10,'white','filled'); 
     axis equal;
     axis([XMIN XMAX YMIN YMAX]) 
     axis off;
@@ -40,7 +40,9 @@ for i = 1:length(z1)
     f = getframe(gca);
     f = frame2im(f); 
     f = rgb2gray(f);
-    imwrite(f, ['../tmp/' num2str(i) '.jpg']);    
+    imwrite(f, ['../tmp/' num2str(i) '.jpg']);
     
 end
+    pause(0.1);
+    close;
 end
