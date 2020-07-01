ddc383e2f0b3f5ec9dd14264c53d3e8f013bfe9e
Longqian Huang <longqianh@gmail.com>
2020-07-01 13:08:57 +0800

    Update MAIN.m

--- 3d-gray/MAIN.m
+++ 3d-gray/MAIN.m
@@ -1,62 +1,65 @@
+clear;clc;clf;
 %  parameters 
 M = 1920; N = 1080; % slm resolution: horizontal and vertical pixels
 slices=100; % number of slices
 m0 = 0.5 ;  % image zoom factor: to prevent image superposition
-lambda=0.532e-3; % wavelength of the light , 532nm for green light
-z0 = 800;  %distance between the defracted plane and observation screen
+lambda=532e-6; % wavelength of the light , 532nm = 532e-6 mm for green light
+z0 = 800;  %distance between the defracted plane and observation screen / mm
 
-depth=30;  % depth
-pix=0.008;  % unit pixel width
-iter_num=20;     % number of iterations
-
+depth= 30;  % depth / mm
+pix=0.008;  % unit pixel width / mm
+iter_times=20;     % iteration times
+z=(1:slices)/slices*depth+z0; % different diffraction distance because of the depth of the object
 
 %  load data
 model = load('../datas/bunny.txt');
 
 
-%  cut pieces
-cut_pieces(model,slices); 
+%  cut pieces and get the slices
+cutted=0;
+if cutted==0
+    cut_pieces(model,slices); 
+end
 
-XY0=cell(slices,1); %pack the slices 
-z=(1:slices)/slices*depth+z0; % different diffraction distance because of the depth of the object
+%pack the slices 
+XY0=cell(slices,1); 
 for i=1:slices
     XY0{i} = imread(['../tmp/' num2str(i) '.jpg']);
 end
 
-%  resize the slices and add random phase
+%  resize the slices and add random phase on each slice
 [U0,A0,xx0,yy0,xx,yy]=initialize(XY0,M,N,m0,lambda,z0,pix);
 
 U_slms=cell(slices,1);
 U_pics=cell(slices,1);
-PhaseGraphs=cell(slices,1);
-times=(iter_num+1)*slices;
 
-for iter=1:iter_num+1  
+% GS iteration
+for iter=1:iter_times
     U_slm=zeros(N,M);
     for i=1:slices
-        U_slms{i}=s_fft(M,N,lambda,z(i),xx0,yy0,xx,yy,U0{i});
+        U_slms{i}=s_fft(U0{i},M,N,lambda,z(i),xx0,yy0,xx,yy);
         U_slm=U_slm+U_slms{i};% complex applitudes superposition 
-        
     end
     abs(U_slm)
-    Phase_slm=angle(U_slm)+pi;% takeout angle to get pahse graph, angle is range from -pi to pi
+    phase=angle(U_slm)+pi;% takeout angle to get pahse graph, angle is range from -pi to pi
     
     for i=1:slices
-        U_pics{i}=i_fft(M,N,lambda,z(i),xx0,yy0,xx,yy,Phase_slm);
-        PhaseGraphs{i}=angle(U_pics{i});
-        U0{i}=A0{i}.*exp(1i*PhaseGraphs{i});
+        U_pics{i}=i_fft(exp(1i.*(phase)),M,N,lambda,z(i),xx0,yy0,xx,yy);
+        U0{i}=A0{i}.*exp(1i*(angle(U_pics{i})+pi));
+        
     end
-    disp(iter*slices/times); % running progress 
+    disp(iter/iter_times);
 end
 
-Ih=uint8(Phase_slm/2/pi*255);
+PhaseGraph=uint8(phase/2/pi*255);
 % [filename,folder] = uiputfile('*.bmp','save graph','holo-graph.bmp');
-imwrite(Ih,'phase-only-img.bmp');%../holo-graph/holo-graph
+imwrite(PhaseGraph,'phase-only-img.bmp');%../holo-graph/holo-graph
 
 
 brightness=3;
-pic_num=i;
-res=brightness*abs(U_pics{pic_num})/max(max(abs(U_pics{pic_num}))); 
-res=imresize(res,[1000 1000]);
-figure;
-imshow(res);
+
+pic_num=1;
+simu_res=brightness*abs(U_pics{pic_num})/max(max(abs(U_pics{pic_num}))); 
+simu_res=imresize(simu_res,[1000 1000]);
+imshow(simu_res);
+
