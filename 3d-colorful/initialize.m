782a24943d90fe895761a6e572b2c6013ff38fea
Longqian Huang <longqianh@gmail.com>
2020-09-25 08:29:09 +0800

    xx0{k}-->xx0{k,i}

--- 3d-colorful/initialize.m
+++ 3d-colorful/initialize.m
@@ -21,7 +21,7 @@ n = 1:N;
 m = 1:M;
 LM=pix*M;
 LN=pix*N;
-m0=0.5*L(3)./L; % zooming factor
+
 
 % from the observation plane do Fresnel diffraction integral to slm
 x = ((m-1)/M-0.5)*LM;
@@ -29,26 +29,33 @@ y = ((n-1)/N-0.5)*LN;
 [xx,yy] = meshgrid(x,y);
 
 U0=cell(3,slices,1); % contain complex amplitudes of each piece in three channels
-xx0s=cell(3,1);
-yy0s=cell(3,1);
+xx0s=cell(3,slices,1);
+yy0s=cell(3,slices,1);
 
 
  for k=1:3
-        x0 = ((m-1)/M-0.5)*L(k);
-        y0 = ((n-1)/N-0.5)*L(k);
-        [xx0,yy0] = meshgrid(x0,y0);
-        xx0s{k}=xx0;
-        yy0s{k}=yy0;    
+%         x0 = ((m-1)/M-0.5)*L{k};
+%         y0 = ((n-1)/N-0.5)*L{k};
+%         [xx0,yy0] = meshgrid(x0,y0);
+%         xx0s{k}=xx0;
+%         yy0s{k}=yy0; 
+        m0=0.5*L{3}./L{3}; % zooming factor
         for i=1:slices
-                    img = imread(['./tmp/' num2str(i) '.jpg']);
-                    img=img(:,:,k); % take k's channel out
-                    img = imresize(img,[N,M]); %resize the piece 
-                    img = imresize(img,m0(k)); % zooming automatically
-                    [N1,M1] = size(img); % size of the piece after the adjustment
-                    X = zeros(N,M);
-                    X(N/2-N1/2+1 : N/2+N1/2, M/2-M1/2+1 : M/2+M1/2) = img; % piece with zero-paddings
-                    random_phase = rand(N,M)*2*pi; %add random phase
-                    U0{k,i}=double(X).*exp(1i.*random_phase); 
+                x0 = ((m-1)/M-0.5)*L{k}(i);
+                y0 = ((n-1)/N-0.5)*L{k}(i);
+                [xx0,yy0] = meshgrid(x0,y0);
+                xx0s{k,i}=xx0;
+                yy0s{k,i}=yy0; 
+                img = imread(['./tmp/' num2str(i) '.jpg']);
+                img=img(:,:,k); % take k's channel out
+                img = imresize(img,[N,M]); %resize the piece 
+%                 img = imresize(img,[N,M/3]); %resize the piece 
+                img = imresize(img,m0(i)); % zooming automatically
+                [N1,M1] = size(img); % size of the piece after the adjustment
+                X = zeros(N,M);
+                X(N/2-N1/2+1 : N/2+N1/2, M/2-M1/2+1 : M/2+M1/2) = img; % piece with zero-paddings
+                random_phase = rand(N,M)*2*pi; %add random phase
+                U0{k,i}=double(X).*exp(1i.*random_phase); 
         end
  end
 end
--- 3d-colorful/main.m
+++ 3d-colorful/main.m
@@ -19,7 +19,11 @@ z=(1:slices)/slices*depth+z0; % different diffraction distance because of the de
 
 lambdas=[635e-6 520e-6 450e-6];
 % L=lambdas*z0/pix;
-L=lambdas.*z/pix;
+L=cell(3,1);
+for i=1:3
+    L{i}=lambdas(i)*z/pix;
+end
+
 % dist/（lambda·z）
 cut_pieces(model,mcolor,slices); 
 [U0,xx0s,yy0s,xx,yy]=initialize(slices,M,N,L,pix); 
@@ -27,11 +31,6 @@ cut_pieces(model,mcolor,slices);
 % GS iteration
 pog=zeros(N,M);
 rcst=cell(3,1);
-% move=[-1,0,1];
-% movex=[0,0,0];
-% movey=[0,0,0];
-% movex=[3 0 -1];
-% movey=[2 1 0];
 
 for k = 1:3
     
@@ -39,30 +38,31 @@ for k = 1:3
         U_slm=zeros(N,M);
 
         for i=1:slices
-             tmp=s_fft(U0{k,i},M,N,lambdas(k),z(i),xx0s{k},yy0s{k},xx,yy);
+             tmp=s_fft(U0{k,i},M,N,lambdas(k),z(i),xx0s{k,i},yy0s{k,i},xx,yy);
             U_slm=U_slm+tmp;% complex applitudes superposition 
         end
         
         phase=angle(U_slm)+pi;% takeout angle to get phase graph, angle is range from -pi to pi
         blaz=2*pi/(2*pix)*xx0s{k};
-        blazdx=2*pi/(2*pix)*(L(2)-L(k))/L(k)*xx0s{k};
-        blazdy=2*pi/(2*pix)*(L(2)-L(k))/L(k)*yy0s{k};
+        blazdx=2*pi/(2*pix)*(L{2}-L{k})/L{k}*xx0s{k,i};
+        blazdy=2*pi/(2*pix)*(L{2}-L{k})/L{k}*yy0s{k,i};
         phase=mod(phase+blaz+blazdx+blazdy,2*pi);
         
         for i=1:slices
-            tmp=i_fft(exp(1i.*(phase-pi)),M,N,lambdas(k),z(i),xx0s{k},yy0s{k},xx,yy);
+            tmp=i_fft(exp(1i.*(phase-pi)),M,N,lambdas(k),z(i),xx0s{k,i},yy0s{k,i},xx,yy);
             if i==slices
                 rcst{k}=tmp;
             end
             U0{k,i}=abs(U0{k,i}).*exp(1i*(angle(tmp))); 
         end
-        disp(iter*k/(iter_times*k));
+%         disp(iter*k/(iter_times*k));
     end
     
     pog(:,1+M*(k-1)/3:M*k/3)=phase(:,1+M*(k-1)/3:M*k/3);
+    disp(k/3);
 end
 pog=uint8(pog/2/pi*255);
 imwrite(pog,'main.bmp');%../holo-graph/holo-graph
 
 % simulation of reconstruction
-recst(rcst,L);
+% recst(rcst,L);
